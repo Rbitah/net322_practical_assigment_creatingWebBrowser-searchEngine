af995511a5b92b9108fc964107220663c33e138a
Rbitah <bsc-35-21@unima.ac.mw>
2025-05-08 19:45:36 +0200

    added not found temp to search on other search engine

--- webbrower.py
+++ webbrower.py
@@ -7,7 +7,10 @@ class WebBrowser(QMainWindow):
     def __init__(self):
         super(WebBrowser,self).__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("https://www.google.com"))
+        
+        self.base_url = "http://localhost:8085"
+        
+        self.browser.setUrl(QUrl(self.base_url))
         self.setCentralWidget(self.browser)
         self.showMaximized()
         
@@ -20,7 +23,34 @@ class WebBrowser(QMainWindow):
         fwdButton = QAction("Forward",self)
         fwdButton.triggered.connect(self.browser.forward)
         navbar.addAction(fwdButton)
+        self.link_Input = QLineEdit()
+        self.link_Input.setPlaceholderText("Enter URL")
+        self.link_Input.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.link_Input)
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_to_url(self):
+        url = self.link_Input.text()
+        if not url.startswith("http"):
+            if url == "index":
+             url = f"{self.base_url}"
+             self.browser.setUrl(QUrl(url))
+        elif url == "register":
+            url = f"{self.base_url}/register"
+            self.browser.setUrl(QUrl(url))
+            
+        elif url.startswith("http://") or url.startswith("https://"):
+            url = url
+            self.browser.setUrl(QUrl(url))    
+        else:
+            self.last_query = url
+            notFound= "Not Found"
+            self.browser.setHtml(notFound)
         
+    def update_url(self, url):
+        self.link_Input.setText(url.toString())    
+ 
+ 
 def main():        
         app = QApplication(sys.argv) 
         QApplication.setApplicationName("Web Browser")
